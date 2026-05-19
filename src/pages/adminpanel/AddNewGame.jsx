b5936cfd010447f6879bc6caa577936fed470a7f
mainamuchiru <mainaephantus@gmail.com>
2026-05-19 15:26:35 +0300

    On CRUD branch I Created two folders in src folder namely hooks and service, hooks holds hooks while, services holds api fetching call

--- src/pages/adminpanel/AddNewGame.jsx
+++ src/pages/adminpanel/AddNewGame.jsx
@@ -1,6 +1,45 @@
+import { useState } from "react";
 import ProductForm from "../../components/ProductForm";
+import useProducts from "../../hooks/useProducts";
+
 function AddNewGame() {
-  return <ProductForm />;
+  const { createProduct } = useProducts();
+  const [gameData, setGameData] = useState({
+    name: "",
+    description: "",
+    releasedate: "",
+    gamegenre: "",
+    platform: "",
+    image: "",
+    imageurl: "",
+    price: "",
+    learnmore: "",
+  });
+
+  function handleChange(e) {
+    const { name, value, files } = e.target;
+
+    setGameData({
+      ...gameData,
+      [name]: files ? URL.createObjectURL(files[0]) : value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await createProduct(gameData);
+    alert(
+      `${gameData.name} Genre: ${gameData.gamegenre} Has been added succesfully`,
+    );
+  }
+
+  return (
+    <ProductForm
+      products={gameData}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  );
 }
 
 export default AddNewGame;
---
+++ src/services/productService.js
@@ -0,0 +1,38 @@
+// services/productService.js
+
+const API_URL = "http://localhost:3000/games";
+
+export async function getProducts() {
+  const response = await fetch(API_URL);
+  return response.json();
+}
+
+export async function addProduct(product) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  });
+
+  return response.json();
+}
+
+export async function updateProduct(id, updatedProduct) {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedProduct),
+  });
+
+  return response.json();
+}
+
+export async function deleteProduct(id) {
+  await fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+  });
+}
